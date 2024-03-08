946a224f8b2db82c0a9b365eafae563d621dfba3
Ludovic <ludovic.pons-de-guigues@etu.univ-amu.fr>
2024-03-08 08:43:18 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'le petit prince',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
